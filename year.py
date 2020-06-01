8a7dd15c989134b2985bea54907c34212cea3fad
inshimingyue <66257258+inshimingyue@users.noreply.github.com>
2020-06-01 16:51:08 +0200

    Update year.py

--- year.py
+++ year.py
@@ -1,4 +1,13 @@
-while expression:
-   while expression:
-      statement(s)
-   statement(s)
+#!/usr/bin/env python 
+# -*- coding:utf-8 -*-
+print('---------i am yu--------')
+temp = input("不防猜一猜我现在心里想的是哪个数字：")
+guess = int(temp)
+if guess == 8:
+    print("呀，你是我心里的蛔虫吗？！")
+    print("哼，猜中了也没有奖励！")
+
+else:
+    print("猜错啦，我现在心里想的是8！")
+
+print("游戏结束，不玩啦O(∩_∩)O")
